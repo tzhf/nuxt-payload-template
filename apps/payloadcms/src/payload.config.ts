b7596d15551b558a1881af9fdbabfc93ae3eb65e
Théo ZAHAF <theo.zahaf@gmail.com>
2026-07-10 23:36:59 +0200

    vercel rewrite path

--- apps/payloadcms/src/payload.config.ts
+++ apps/payloadcms/src/payload.config.ts
@@ -18,6 +18,17 @@ const __dirname = path.dirname(__filename)
 
 export default buildConfig({
   serverURL: process.env.NEXT_PUBLIC_SITE_URL,
+  // Add your Nuxt frontend Vercel URL to both of these arrays
+  cors: [
+    'https://my-project-frontend-taupe.vercel.app',
+    process.env.NEXT_PUBLIC_SITE_URL,
+  ].filter(Boolean),
+
+  csrf: [
+    'https://my-project-frontend-taupe.vercel.app',
+    process.env.NEXT_PUBLIC_SITE_URL,
+  ].filter(Boolean),
+
   secret: process.env.PAYLOAD_SECRET,
   debug: process.env.NODE_ENV !== 'production',
   admin: {
